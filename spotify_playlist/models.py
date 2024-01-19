4d67928522471c99a6e5d0cc534b954221218a56
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-19 18:54:40 -0500

    making the models

--- spotify_playlist/models.py
+++ spotify_playlist/models.py
@@ -1,3 +1,15 @@
+# spotify_playlist/models.py
+
 from django.db import models
 
-# Create your models here.
+class Inquiry(models.Model):
+    artist1 = models.CharField(max_length=255)
+    artist2 = models.CharField(max_length=255)
+    artist3 = models.CharField(max_length=255)
+    date_of_inquiry = models.DateTimeField('date published')
+    vibe = models.CharField(max_length=255)
+
+class Song(models.Model):
+    song_title = models.CharField(max_length=255)
+    artist_name = models.CharField(max_length=255)
+    inquiry = models.ForeignKey(Inquiry, related_name='songs', on_delete=models.CASCADE)
