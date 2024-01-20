9569ac786a125f6a8454dcfaddb4cb35ad6425f9
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-19 19:45:45 -0500

    f

--- spot_project/urls.py
+++ spot_project/urls.py
@@ -2,8 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from spotify_playlist.views import inquiries_index  # Import the inquiries_index view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inquiries_index, name='inquiries_index'),
     path('', include('spotify_playlist.urls')),  # Include the app's URLs with an empty path
 ]
