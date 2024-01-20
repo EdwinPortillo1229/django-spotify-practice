701127c2cfe1a41f3714295383247d9086e5fbe2
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-20 11:46:11 -0500

    f

--- spotify_playlist/models.py
+++ spotify_playlist/models.py
@@ -2,12 +2,17 @@
 
 from django.db import models
 
+class SpotifyUser(models.Model):
+    access_token = models.CharField(max_length=255)
+
 class Inquiry(models.Model):
     artist1 = models.CharField(max_length=255)
     artist2 = models.CharField(max_length=255)
     artist3 = models.CharField(max_length=255)
     date_of_inquiry = models.DateTimeField('date published')
     vibe = models.CharField(max_length=255)
+    spotify_user = models.ForeignKey(SpotifyUser, related_name='inquiries', on_delete=models.CASCADE, default=None, null=True)
+
 
 class Song(models.Model):
     song_title = models.CharField(max_length=255)
