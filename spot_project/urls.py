2ccd4fab02d64d90a1071740758e577361d4e468
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-01-20 08:57:46 -0500

    wip

--- spot_project/urls.py
+++ spot_project/urls.py
@@ -8,5 +8,6 @@ from spotify_playlist.views import inquiries_index  # Import the inquiries_index
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inquiries_index, name='inquiries_index'),
-    path('', include('spotify_playlist.urls')),  # Include the app's URLs with an empty path
+    path('', include('spotify_playlist.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
