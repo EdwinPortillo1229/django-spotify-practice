98045f191b379259e19e5f01a08e856fea2ced14
Edwin Portillo <edwinportillo@Edwins-MacBook-Pro.local>
2024-02-11 11:35:16 -0500

    wip

--- spotify_playlist/static/javascript/create_inquiry.js
+++ spotify_playlist/static/javascript/create_inquiry.js
@@ -2,12 +2,88 @@ document.addEventListener('DOMContentLoaded', function() {
     var artist1Input = document.querySelector('input[name="artist1"]');
     var artist2Input = document.querySelector('input[name="artist2"]');
     var artist3Input = document.querySelector('input[name="artist3"]');
+    var submitButton = document.querySelector('#submit-button');
 
     artist1Input.addEventListener('change', function() {
-        console.log('change');
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
     });
     artist1Input.addEventListener('keyup', function() {
-        console.log('keyup');
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
     });
 
+    artist2Input.addEventListener('change', function() {
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
+    });
+
+    artist2Input.addEventListener('keyup', function() {
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
+    });
+
+    artist3Input.addEventListener('change', function() {
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
+    });
+
+    artist3Input.addEventListener('keyup', function() {
+        const artist1 = artist1Input.value;
+        const artist2 = artist2Input.value;
+        const artist3 = artist3Input.value;
+
+        if (artist1 && artist2 && artist3) {
+            submitButton.disabled = false;
+            submitButton.classList.remove('disabled');
+        } else {
+            submitButton.disabled = true;
+            submitButton.classList.add('disabled');
+        }
+    });
 });
